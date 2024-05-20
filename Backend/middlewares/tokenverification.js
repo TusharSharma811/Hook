13c1c26cc5f51329afb55c64ea79c5c0fe6542f9
TusharSharma811 <sharmatushar811@gmail.com>
2024-05-20 15:01:50 +0530

    middleware added

---
+++ Backend/middlewares/tokenverification.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken" 
+import env from "dotenv"
+
+
+const tokenverification = (req , res, next )=>{
+    env.config() ;
+    const header = req.headers.Authorization ;
+    const token = header.split(" ") ;
+    const state = jwt.verify(token[1] , process.env.salt ) ;
+    if(!state){
+        res.send({message : "Unautorised user"});
+
+    }
+    else{
+        next() ;
+    }
+}
+
+export default tokenverification
